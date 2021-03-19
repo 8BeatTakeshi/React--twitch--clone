91d6501dd34938c648ec7234b5a55820c02563ea
8BeatTakeshi <d.nakamura75@gmail.com>
2021-03-19 13:17:29 +0100

    feat: install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Games from './components/Games/Games';
+import TopStreams from './components/TopStreams/TopStreams';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Sidebar />
-      <Games />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Sidebar />
+
+        <Switch>
+          <Route exact path="/" component={Games} />
+          <Route exact path="/top-streams" component={TopStreams} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/TopStreams/TopStreams.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './TopStreams.css';
+
+const TopStreams = () => {
+  return <h1>Top Streams</h1>;
+};
+
+export default TopStreams;
