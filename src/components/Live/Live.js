971daba78989e90ed7f4db968ea7cae62b95c78d
8BeatTakeshi <d.nakamura75@gmail.com>
2021-03-20 19:42:37 +0100

    feat: add content in Live and style it

--- src/components/Live/Live.js
+++ src/components/Live/Live.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactTwitchEmbedVideo from 'react-twitch-embed-video';
+/* useParams: This hook gives us access to the params of that particular route. params
+are parameters whose values are set dynamically in a URL.
+Usually, the way we access the params in previous versions of react-router
+was through the match props passed to the component. */
 import { useParams } from 'react-router-dom';
+import api from '../../api';
 import './Live.css';
 
 const Live = () => {
   let { slug } = useParams();
-  console.log(slug);
+
+  const [infoStream, setInfoStream] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await api.get(
+        `https://api.twitch.tv/helix/streams?user_login=${slug}`
+      );
+      console.log(result);
+      setInfoStream(result.data.data[0]);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="container">
       <ReactTwitchEmbedVideo height="754" width="100%" channel={slug} />
+      <div className="container_info">
+        <div className="container_info_title">{infoStream.title}</div>
+        <div className="container_info_viewer">
+          Viewers : {infoStream.viewer_count}
+        </div>
+        <div className="container_info_streamer">
+          Streamer : {infoStream.user_name}, &nbsp; Langue :{' '}
+          {infoStream.language}
+        </div>
+        <div className="container_info_game">Jeu : {infoStream.game_name}</div>
+      </div>
     </div>
   );
 };
