5624e7b6ed6a3e0319678281867dc3e089cada51
8BeatTakeshi <d.nakamura75@gmail.com>
2021-03-21 16:04:51 +0100

    feat: make the Header menu responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,49 +1,84 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import logo from './TwitchIcon.svg';
-import SearchIcon from './SearchIcon.svg';
-import MenuIcon from './MenuIcon.svg';
+import searchIcon from './SearchIcon.svg';
+import menuIcon from './MenuIcon.svg';
+import cancelIcon from './CancelIcon.svg';
 import './Header.css';
 
 const Header = () => {
+  const [menu, setMenu] = useState(false); // Responsive Menu
+  const [smallScreen, setSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 900px');
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  });
+
+  const handleMediaQueryChange = (mediaQuery) => {
+    mediaQuery.matches ? setSmallScreen(true) : setSmallScreen(false);
+  };
+
+  const toggleNavRes = () => {
+    setMenu(!menu);
+  };
+
+  const hideMenu = () => {
+    if (menu) {
+      setMenu(!menu);
+    }
+  };
+
   return (
     <div>
       <nav className="header_top">
-        <ul className="menuList">
-          <li className="navLink">
-            <Link className="link" to="/">
-              <img src={logo} alt="Logo Twitch" className="logo" />
-            </Link>
-          </li>
+        {(menu || !smallScreen) && (
+          <ul className="menuList">
+            <li onClick={hideMenu} className="navLink">
+              <Link className="link" to="/">
+                <img src={logo} alt="Logo Twitch" className="logo" />
+              </Link>
+            </li>
 
-          <li className="navLink">
-            <Link className="link" to="/">
-              Top Games{' '}
-            </Link>
-          </li>
+            <li onClick={hideMenu} className="navLink">
+              <Link className="link" to="/">
+                Top Games{' '}
+              </Link>
+            </li>
 
-          <li className="navLink">
-            <Link className="link" to="/top-streams">
-              Top Streams
-            </Link>
-          </li>
-          <li className="navLink">
-            <form className="formSubmit">
-              <input type="text" className="searchInput" />
-              <button type="submit">
-                <img
-                  src={SearchIcon}
-                  alt="Icone loupe"
-                  className="searchIcon"
-                />
-              </button>
-            </form>
-          </li>
-        </ul>
+            <li onClick={hideMenu} className="navLink">
+              <Link className="link" to="/top-streams">
+                Top Streams
+              </Link>
+            </li>
+            <li className="navLink">
+              <form className="formSubmit">
+                <input type="text" className="searchInput" />
+                <button type="submit">
+                  <img
+                    src={searchIcon}
+                    alt="Icone loupe"
+                    className="searchIcon"
+                  />
+                </button>
+              </form>
+            </li>
+          </ul>
+        )}
       </nav>
 
       <div className="menuResponsive-Btn">
-        <img src={MenuIcon} alt="Icone menu responsive" className="menuIcon" />
+        <img
+          onClick={toggleNavRes}
+          src={menu ? cancelIcon : menuIcon}
+          alt="Icone menu responsive"
+          className="menuIcon"
+        />
       </div>
     </div>
   );
